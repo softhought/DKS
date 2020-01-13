bca836e0f00881a8f77aec15cbf5323a793b7625
Mithilesh Routh <kumarrouth93@gmail.com>
2020-01-13 15:29:47 +0530

    Order Ui
    Order Ui

---
+++ application/views/dashboard/order/order-view.php
@@ -0,0 +1,96 @@
+<div class="order-view-panel layout-box-content-format1">
+    <div class="left-order-panel">
+        <div class="card card-primary">
+            <div class="card-header box-shdw">
+              <h3 class="card-title">Order</h3>
+
+              <div class="btn-group btn-group-sm float-right" role="group" aria-label="MoreActionButtons">
+                <!-- <button type="button" class="btn btn-default"><i class="fas fa-plus"></i> Add </button> -->
+                <button type="button" class="btn btn-default"><i class="fas fa-clipboard-list"></i>Order List</button>
+                <!-- <button type="button" class="btn btn-default" data-toggle="modal" data-target="#codegeneration_modal" id="codegenbtn"><i class="fas fa-cog"></i> Generate Code</button> -->
+              </div>
+            </div>
+
+            <div class="card-body">
+                <div class="member-info formblock-box">
+                    <h3 class="form-block-subtitle"><i class="fas fa-angle-double-right"></i> Member Info</h3>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <div class="input-group input-group-sm">
+                                    <input type="text" class="form-control" placeholder="Enter Member Code" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <div class="input-group input-group-sm">
+                                    <input type="text" class="form-control" placeholder="Member Name" readonly>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <div class="input-group input-group-sm">
+                                    <input type="text" class="form-control" placeholder="Member Mobile" readonly >
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- End of row -->
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <!-- 
+                            https://dribbble.com/shots/3730167-Foms-Best-Restaurant-Point-of-Sale-Systems    
+                            <label for="category">Location</label> -->
+                            <div class="form-group">
+                              <div class="input-group input-group-sm">
+                                    <select class="form-control select2" style="width: 100%;">
+                                      <option value=''>Choose Location</option>
+                                      <option value="1">Location 1</option>
+                                    </select>
+                              </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <!-- <label for="category">Category</label> -->
+                            <div class="form-group">
+                              <div class="input-group input-group-sm">
+                                    <select class="form-control select2" id="category" name="category" style="width: 100%;">
+                                      <option value=''>Choose Category</option>
+                                      <option value="1">Catg 1</option>
+                                    </select>
+                              </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <!-- <label for="category">Waiter</label> -->
+                            <div class="form-group">
+                              <div class="input-group input-group-sm">
+                                    <select class="form-control select2" style="width: 100%;">
+                                      <option value=''>Choose Waiter</option>
+                                      <option value="1">Waiter 1</option>
+                                    </select>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        
+    </div><!-- End of Left Order Panel -->
+    <div class="right-order-panel">
+        <div class="card card-primary">
+            <div class="card-header box-shdw">
+              <h3 class="card-title">Order Summary</h3>
+            </div>
+        </div>
+        <div class="card-body">
+            gggggggjfhsg
+        </div>
+    </div><!-- End of Right Order Panel -->
+</div><!-- End of order-view-panel ---->
